47b88dd74b948e35a5e1e6ec106626bc6765da05
AbderazakAmiar <abderazak Amiar>
2020-01-29 08:31:08 +0100

    test commit

--- app/src/main/java/dz/apps/constraintsetexample/MainActivity.java
+++ app/src/main/java/dz/apps/constraintsetexample/MainActivity.java
@@ -2,12 +2,14 @@ package dz.apps.constraintsetexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+	Log.i("hello", "onCreate: ");
+}
 }
